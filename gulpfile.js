2c9bffc4d2f8c1eb2b288cb3720cb5e6c1030af4
Wat Chun Pang Gilbert <gilbert.wat@getthingsdone.hk>
2014-01-30 10:53:08 +0800

    prep CNY dev

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
-var gutil = require('gulp-util');
+var gulputil = require('gulp-util');
+var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin');
 
 gulp.task('default', function(){
   // place code for your default task here
