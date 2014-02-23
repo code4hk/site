a9dcd25fe9a91cd41f027edda3f3811add188391
lcy <lauchunyin@gmail.com>
2014-02-23 18:22:39 +0800

    for gulp build bower

--- gulpfile.js
+++ gulpfile.js
@@ -8,10 +8,21 @@ var livereload = require('gulp-livereload');
 var concat = require('gulp-concat');
 var less = require('gulp-less');
 
+var bower = require('gulp-bower');
+
 server = lr();
 
 gulp.task('default', function(){
   // place code for your default task here
+});
+gulp.task('build', function(){
+    gulp.src('less/*.less')
+        .pipe(watch())
+        .pipe(less())
+        .pipe(gulp.dest('app/css/'));
+
+  bower()
+    .pipe(gulp.dest('app/lib/'));
 });
 
 gulp.task('default', ['listen'], function() {
