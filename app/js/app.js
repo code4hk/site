2ebbcaee31d8f286f8fd9b0bffc5930039661dfc
lcy <lauchunyin@gmail.com>
2014-03-16 23:45:18 +0800

    add lifestream

--- app/js/app.js
+++ app/js/app.js
@@ -2,35 +2,60 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('site', [
-    'ngRoute',
-    // 'ui.bootstrap',
-    'myApp.filters',
-    'myApp.services',
-    'myApp.directives',
-    'site.controllers'
-]).
-config(['$routeProvider',
-    function($routeProvider) {
-        $routeProvider.when('/view1', {
-            templateUrl: 'partials/partial1.html',
-            controller: 'MyCtrl1'
-        });
-        $routeProvider.when('/view2', {
-            templateUrl: 'partials/partial2.html',
-            controller: 'MyCtrl2'
-        });
-        $routeProvider.otherwise({
-            redirectTo: '/view1'
-        });
-    }
-]);
+
+//wrapper here before built min.js can load modules sync
+//this is almond, not true requirejs
+
+require(["angular-lifestream"], function() {
+
+    angular.module('site', [
+        'ngRoute',
+        // 'ui.bootstrap',
+        'myApp.filters',
+        'myApp.services',
+        'myApp.directives',
+        'site.controllers',
+        "angular-lifestream"
+    ]).
+    config(['$routeProvider',
+        function($routeProvider) {
+            $routeProvider.when('/view1', {
+                templateUrl: 'partials/partial1.html',
+                controller: 'MyCtrl1'
+            });
+            $routeProvider.when('/view2', {
+                templateUrl: 'partials/partial2.html',
+                controller: 'MyCtrl2'
+            });
+            $routeProvider.otherwise({
+                redirectTo: '/view1'
+            });
+        }
+    ]);
+
+    angular.module('site.controllers', ['ngSanitize']).
+    controller('lifestreamCtrl', ['$scope',
+        function($scope) {
+
+            $scope.lifestreamConfig = {
+                theme: "lifestream-light-theme",
+                list: [{
+                    service: "github_org",
+                    user: "code4hk",
+                }, {
+                    service: "twitter",
+                    user: "code4hk"
+                }, {
+                    service: "facebook_page",
+                    user: "code4hk"
+                }]
+            };
 
 
-angular.module('site.controllers', []).
-controller('lifestreamCtrl', ['$scope',
-    function($scope) {
+        }
+    ]);
+    angular.bootstrap(document, ["site"]);
 
 
-    }
-]);
+
+});
